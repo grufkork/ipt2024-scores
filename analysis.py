842c90ed6fb8d2fc8d10cb7930da339973a1312c
Viktor Sundström <vsu.forward.unit@gmail.com>
2024-04-06 12:04:40 +0200

    scraping per fight data

--- analysis.py
+++ analysis.py
@@ -16,44 +16,56 @@ import scipy
 import matplotlib.pyplot as plt
 import datetime
 
+#data = pd.read_csv('reduced_scores.csv')#, index_col='AsOfDate', parse_dates=['AsOfDate'])
 data = pd.read_csv('scores.csv')#, index_col='AsOfDate', parse_dates=['AsOfDate'])
-print(data)
+
+
 dfs=[]
 df = data
 for i in df["fightName"].unique():
-    print(i)
     df_i = df.loc[df["fightName"]==i]
-    #print(df_i)
     dfs.append(df_i.copy())
 
 print("Analysis starts now ...")
 
 # juror	country	fight	role	score	participant	problem	round	fightName
 
-round_1_score = 0
-round_2_score = 0
-round_3_score = 0
+round_scores = np.array([0,0,0])
 
-def calc_grade_for_fight(fight, col, val_array):
-    for index, i in fight.iterrows():
-        for i in val_array:
+def calc_stats_for_fight(fight, col, val_array,average):
+    ret_arr = [0 for x in val_array]
+    num_entries =  [0 for x in val_array]
 
-        fight[]
+    for index, f in fight.iterrows():
+        for i in range(len(val_array)):
+            val = val_array[i]
+            if f[col] == val:
+                ret_arr[i] += f["score"]
+                num_entries[i] += 1
 
+    if average:
+        for i in range(len(val_array)):
+            ret_arr[i] = ret_arr[i] / num_entries[i]
+    
+    return ret_arr
 
 for fight in dfs:
-    #print(fight)
+    round_scores = round_scores + np.array(calc_stats_for_fight(fight, "round", [1,2,3], average=True))/len(dfs)
 
+print(round_scores)
+    
+'''
     # Does later fights perform better?
-    for index, i in fight.iterrows():#fight.loc[df["round"]==1]:
+    #for index, i in fight.iterrows():#fight.loc[df["round"]==1]:
+        
         
         if i["round"] == 1:
                  
         elif i["round"] == 2:
         
         print(i["round"])
-
+        
         #df_i = df.loc[df["fightName"]==i]
 
 
-
+'''
